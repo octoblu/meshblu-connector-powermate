9a3e64a1bb0477685085fca54decae45db91e1a4
Jade Meskill <jade.meskill@gmail.com>
2017-06-13 12:48:57 -0700

    remove fancy check path

--- index.js
+++ index.js
@@ -1,14 +1,3 @@
 "use strict"
 
-var path = require("path")
-var pathExists = require("path-exists")
-var connector = null
-
-if (pathExists.sync(path.resolve("./lib/index.js"))) {
-  connector = require("./lib")
-} else {
-  require("coffee-script/register")
-  connector = require("./src")
-}
-
-module.exports = connector
+module.exports = require('./lib/index')
